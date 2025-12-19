9ad6e2dc6437ae3035fbc87ad6c866fc012726a2
savantatvasoft <savan.soni@tatvasoft.com>
2025-12-19 12:36:28 +0530

    Add valid folder and file name

---
+++ ValidateUser/Features/Registration/Controller/RegistrationVC.swift
@@ -0,0 +1,29 @@
+//
+//  RegistrationVC.swift
+//  ValidateUser
+//
+//  Created by MACM72 on 19/12/25.
+//
+
+import UIKit
+
+class RegistrationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("✅ RegistrationVC has loaded successfully!")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
