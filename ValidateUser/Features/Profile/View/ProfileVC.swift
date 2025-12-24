c4a97c7d81284dd9e8ee85660e0f7bea5a986601
savantatvasoft <savan.soni@tatvasoft.com>
2025-12-24 19:39:58 +0530

    remove comments and spces

--- ValidateUser/Features/Profile/View/ProfileVC.swift
+++ ValidateUser/Features/Profile/View/ProfileVC.swift
@@ -27,24 +27,16 @@ class ProfileVC: UIViewController {
     private func setupUI() {
         guard let vm = viewModel else { return }
         
-        // 1. Direct assignments from the model inside the VM
-        userNameTitle.text = vm.data.name.isEmpty ? "Lorem ipsum" : "\(vm.data.name)"
+        userNameTitle.text = vm.data.name
         userImage.image = vm.getProfileImage()
-        
-        // 2. Setting your custom views
-        userName.label.text = vm.data.name.isEmpty ? "Lorem ipsum" : "\(vm.data.name)"
+        userName.label.text = vm.data.name
         userEmail.label.text = vm.data.email
         userPhoneNumber.label.text = vm.data.phone
         userLinkedInUrl.label.text = vm.data.linkedinUrl
-        userDescription.text = vm.data.description.isEmpty ? "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda." : "\(vm.data.description)"
+        userDescription.text = vm.data.description
         
         userEmail.image.image = UIImage(named: "Email")
         userPhoneNumber.image.image = UIImage(named: "Phone_Call")
         userLinkedInUrl.image.image = UIImage(named: "linked_In")
-//        userLinkedInUrl.image.image = UIImage(named: "About_Us")
-        
-//        // 3. Styling
-//        userImage.layer.cornerRadius = userImage.frame.height / 2
-//        userImage.clipsToBounds = true
     }
 }
--- ValidateUser/Features/Profile/ViewModel/ProfileViewModel.swift
+++ ValidateUser/Features/Profile/ViewModel/ProfileViewModel.swift
@@ -16,15 +16,11 @@ class ProfileViewModel {
     }
     
     func getProfileImage() -> UIImage? {
-            // 1. Check if the string exists
+            
             if let base64String = data.userImage,
-               // 2. Convert string to Data
                let imageData = Data(base64Encoded: base64String) {
-                // 3. Return the image from the registration data
                 return UIImage(data: imageData)
             }
-            
-            // 4. Default image if no photo was taken
             return UIImage(systemName: "person.circle.fill")
         }
 }
