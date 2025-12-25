f3d81543637fa08b1380b28b87de22b288eecb3e
savantatvasoft <savan.soni@tatvasoft.com>
2025-12-25 17:49:41 +0530

    Fix type issue

--- ValidateUser/Features/Registration/Model/Registration.swift
+++ ValidateUser/Features/Registration/Model/Registration.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 struct Registration {
-    var name: String = ""
-    var email: String = ""
-    var phone: String = ""
-    var dob: String = ""
-    var linkedinUrl: String = ""
-    var description: String = ""
-    var isTermsAccepted: Bool = false
-    var userImage: String? // Base64
+    var name: String?
+    var email: String?
+    var phone: String?
+    var dob: String?
+    var linkedinUrl: String?
+    var description: String?
+    var isTermsAccepted = false
+    var userImage: String?
 }
